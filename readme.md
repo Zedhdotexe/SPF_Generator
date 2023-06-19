1d826968338ad90dfe94c1505155bfbaca83d0f1
zedh.exe <86269309+Zedhdotexe@users.noreply.github.com>
2023-06-19 03:06:03 +0100

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,27 @@
+# SPF Generator
+The SPF Generator is a web application that allows users to generate SPF (Sender Policy Framework) records for their domains. SPF records are used to specify which mail servers are authorized to send emails on behalf of a domain, helping to prevent email spoofing and improve email deliverability.
+</br>
+</br>
+### Features:
+Generate SPF records for multiple domains, IPs, and include domains.
+Copy generated SPF records to the clipboard.
+Responsive design for optimal viewing on different devices.
+No dependencies or external libraries required.
+</br>
+</br>
+### Usage:
+Enter the domains, IPs, and include domains in their respective text areas.
+Click the "Generate SPF" button to generate the SPF record.
+The generated SPF record will be displayed in a popup window.
+Click the "Copy to Clipboard" button to copy the SPF record to the clipboard.
+To close the popup window, click the "Close" button.
+</br>
+</br>
+### Contributions:
+Contributions to the project are welcome! If you encounter any issues or have suggestions for improvements, please open an issue or submit a pull request.
+
+</br>
+</br>
+
+### Acknowledgments:
+The SPF Generator project was inspired by the need for a simple and straightforward tool to generate SPF records. 
---
+++ script.js
@@ -0,0 +1,91 @@
+function generateSPF() {
+  var domainsInput = document.getElementById("domains").value.trim();
+  var ipsInput = document.getElementById("ips").value.trim();
+  var includeInput = document.getElementById("include").value.trim();
+
+  // Check if the domains textarea is empty
+  if (domainsInput === "") {
+    document.getElementById("domains").style.borderColor = "red";
+    return; 
+  } else {
+    document.getElementById("domains").style.borderColor = ""; // Reset the border color
+  }
+
+  var domains = domainsInput
+    .split("\n")
+    .map((domain) => domain.trim())
+    .filter((domain) => domain !== "");
+  var ips = ipsInput
+    .split("\n")
+    .map((ip) => ip.trim())
+    .filter((ip) => ip !== "");
+  var includeDomains = includeInput
+    .split("\n")
+    .map((include) => include.trim())
+    .filter((include) => include !== "");
+
+  var output = domains.join(",");
+
+  if (ips.length > 0) {
+    var formattedIPs = ips.map((ip) => "ip4:" + ip);
+    output += ',TXT,"v=spf1 ' + formattedIPs.join(" ") + " ";
+  }
+
+  if (includeDomains.length > 0) {
+    var includeOutput = includeDomains
+      .map((include) => "include:" + include)
+      .join(" ");
+    output += includeOutput + " ";
+  }
+
+  output += '-all"';
+
+  var popupOutput = document.getElementById("output");
+  popupOutput.textContent = output;
+
+  showPopup();
+}
+
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+  setTimeout(function () {
+    popup.classList.add("show");
+  }, 100);
+}
+
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.classList.remove("show");
+  setTimeout(function () {
+    popup.style.display = "none";
+  }, 300);
+}
+
+function copyOutput() {
+  var popupOutput = document.getElementById("output");
+  var output = popupOutput.textContent;
+
+  var tempInput = document.createElement("textarea");
+  tempInput.value = output;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+
+  var copyButton = document.getElementById("copyButton");
+  copyButton.textContent = "Copied!";
+  setTimeout(function () {
+    copyButton.textContent = "Copy to Clipboard";
+  }, 2000);
+}
+
+function closePopup() {
+  var popup = document.getElementById("popup");
+  popup.classList.remove("show");
+  setTimeout(function () {
+    popup.style.display = "none";
+    var popupOutput = document.getElementById("output");
+    popupOutput.textContent = "";
+  }, 300);
+}
